27bc89737cccb5d5b8baa0a84b2208eabe7c2d3e
Electron847 <seth.conner.weber@gmail.com>
2020-03-02 14:41:19 -0600

    simple converter in Xcode/Swift

--- iOS_Applications/SimpleConverter/SimpleConverter/ViewController.swift
+++ iOS_Applications/SimpleConverter/SimpleConverter/ViewController.swift
@@ -12,9 +12,34 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let url = URL(string: "http://data.fixer.io/api/latest?access_key=cdd30d50d55d64add24b1d2537f85361&format=1")
+        
+        let task = URLSession.shared.dataTask(with: url!)
+        {(data, response, error) in
+            if error != nil
+            {
+                print("ERROR!")
+            }
+            else
+            {
+                if let content = data
+                {
+                    do
+                    {//Array
+                        let myJson = try JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                        if let rates = myJson["rates"] as? NSDictionary
+                        {
+                            print(rates)
+                        }
+                    }
+                    catch {}
+                }
+            }
+        
+        }
+        task.resume()
     }
-
 
 }
 

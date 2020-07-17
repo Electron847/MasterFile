dc7f3ad9118fbcf5f9a2b3682ac8ea0f17ec7e63
Electron847 <seth.conner.weber@gmail.com>
2020-07-17 00:40:45 -0400

    refactor

--- July2020/Chemistry/src/Elements/Lithium.java
+++ July2020/Chemistry/src/Elements/Lithium.java
@@ -1,4 +1,25 @@
 package Elements;
 
-public class Lithium {
+final public class Lithium implements Element {
+    private int _protons = 3;
+    private int _neutrons = 3;
+    private int _electrons = 3;
+    private String _orbConfig = "1s1";
+    public Lithium() {}
+
+    public int protons () {
+        return _protons;
+    }
+    public int neutrons () {
+        return _neutrons;
+    }
+    public int electrons () {
+        return _electrons;
+    }
+    public String orbitalConfig () {
+        return _orbConfig;
+    }
+    public double mass(){
+        return 6.94;
+    }
 }
--- July2020/build.sbt
+++
@@ -1,5 +0,0 @@
-name := "July2020"
-
-version := "0.1"
-
-scalaVersion := "2.12.6"

626f8c506a03894df25d0500b8b4d07a2aa07ffb
Electron847 <seth.conner.weber@gmail.com>
2020-07-21 20:39:58 -0400

    Chemistry: Element Design 1

--- July2020/Chemistry/src/Main/Main.java
+++ July2020/Chemistry/src/Main/Main.java
@@ -5,8 +5,9 @@ public class Main {
     public static void main(String[] args){
         Hydrogen hydrogen1 = new Hydrogen();
         Helium helium1 = new Helium();
+        Lithium lithium1 = new Lithium();
         System.out.println(helium1.electrons());
         System.out.println(hydrogen1.electrons());
-        System.out.println(hydrogen1.orbitalConfig());
+        System.out.println(lithium1.mass());
     }
 }
